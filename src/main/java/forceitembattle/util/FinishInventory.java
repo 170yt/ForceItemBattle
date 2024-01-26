92eba224dc76160d62c1d1a43b6c1fa1492a1315
stupxd <uknownvector@gmail.com>
2024-01-27 00:28:40 +0200

    fix finish inventory maybe

--- src/main/java/forceitembattle/util/FinishInventory.java
+++ src/main/java/forceitembattle/util/FinishInventory.java
@@ -6,13 +6,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import javax.annotation.Nullable;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -31,42 +31,45 @@ public class FinishInventory extends InventoryBuilder {
         /* FILL */
         this.setItems(9, 53, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setDisplayName("§8").addItemFlags(ItemFlag.values()).getItemStack());
 
-        /* Found-Items */
-        final int[] startSlot = {10};
-        final int[] placedItems = {-1};
-        final int[] pagesAmount = {1};
-
-        final int[] currentPage = {0};
 
         if(firstTime) {
             new BukkitRunnable() {
 
+                /* Found-Items */
+                int startSlot = 10;
+                int placedItems = -1;
+                int pagesAmount = 1;
+
                 @Override
                 public void run() {
-                    placedItems[0]++;
+                    placedItems++;
 
-                    if(startSlot[0] == 53) {
+                    if(startSlot == 53) {
                         //check if is even needed to create a new page
                         if(ForceItemBattle.getGamemanager().getItemList(targetPlayer).size() > 35) {
-                            pagesAmount[0]++;
-                            pages.put(pagesAmount[0], getInventory().getContents());
+                            pagesAmount++;
+                            pages.put(pagesAmount, getInventory().getContents());
                             setItems(9, 53, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setDisplayName("§8").addItemFlags(ItemFlag.values()).getItemStack());
-                            startSlot[0] = 10;
+                            startSlot = 10;
                             //setItem(27, new ItemBuilder(Material.RED_STAINED_GLASS_PANE).setDisplayName("§cPrevious Page").addItemFlags(ItemFlag.values()).getItemStack());
                             //setItem(35, new ItemBuilder(Material.LIME_STAINED_GLASS_PANE).setDisplayName("§aNext Page").addItemFlags(ItemFlag.values()).getItemStack());
                         }
                     }
-
-
-                    ForceItem forceItem = ForceItemBattle.getGamemanager().getItemList(targetPlayer).get(placedItems[0]);
-                    setItem(startSlot[0], new ItemBuilder(forceItem.getMaterial()).setDisplayName(WordUtils.capitalize(forceItem.getMaterial().name().replace("_", " ").toLowerCase()) + " §8» §6" + forceItem.getTimeNeeded()).getItemStack());
+                    
+                    List<ForceItem> items = ForceItemBattle.getGamemanager().getItemList(targetPlayer);
+                    if (items.isEmpty()) {
+                        setItem(startSlot, new ItemBuilder(Material.BARRIER).setDisplayName("§cNo Items found").getItemStack());
+                    } else {
+                        ForceItem forceItem = items.get(placedItems);
+                        setItem(startSlot, new ItemBuilder(forceItem.getMaterial()).setDisplayName(WordUtils.capitalize(forceItem.getMaterial().name().replace("_", " ").toLowerCase()) + " §8» §6" + forceItem.getTimeNeeded()).getItemStack());
+                    }
 
                     Bukkit.getOnlinePlayers().forEach(players -> players.playSound(players.getLocation(), Sound.ENTITY_ITEM_PICKUP, 1, 1));
 
-                    if(startSlot[0] == 16 || startSlot[0] == 25 || startSlot[0] == 34 || startSlot[0] == 43) startSlot[0] += 3;
-                    else startSlot[0]++;
+                    if(startSlot == 16 || startSlot == 25 || startSlot == 34 || startSlot == 43) startSlot += 3;
+                    else startSlot++;
 
-                    if(placedItems[0] == ForceItemBattle.getGamemanager().getItemList(targetPlayer).size() - 1) {
+                    if(placedItems >= ForceItemBattle.getGamemanager().getItemList(targetPlayer).size() - 1) {
 
                         new BukkitRunnable() {
 
@@ -77,7 +80,7 @@ public class FinishInventory extends InventoryBuilder {
                                         players.closeInventory();
                                     }
 
-                                    players.sendTitle(ForceItemBattle.getGamemanager().sortByValue(ForceItemBattle.getGamemanager().getScore(), true).size() + ". " + targetPlayer.getName(), "§6" + placedItems[0] + " Items found", 15, 35, 15);
+                                    players.sendTitle(ForceItemBattle.getGamemanager().sortByValue(ForceItemBattle.getGamemanager().getScore(), true).size() + ". " + targetPlayer.getName(), "§6" + placedItems + " Items found", 15, 35, 15);
 
                                 });
 
@@ -94,6 +97,9 @@ public class FinishInventory extends InventoryBuilder {
                 }
             }.runTaskTimer(ForceItemBattle.getInstance(), 0L, 10L);
         } else {
+
+            final int[] currentPage = {0};
+
             //Open Inventory beginning from the first page
             System.out.println(ForceItemBattle.getGamemanager().savedInventory.toString());
             this.getInventory().setContents(ForceItemBattle.getGamemanager().savedInventory.get(targetPlayer.getUniqueId()).get(currentPage[0]));
